c1e2e92928f0802cb7dd5582bbb770fc30f38420
Dinesh A <adepu.dinesh.a@gmail.com>
2017-05-10 00:50:00 +0530

    added tangential overlap integration steps

--- src/integrator.py
+++ src/integrator.py
@@ -22,7 +22,8 @@ class DEMStep(IntegratorStep):
     """
 
     def initialize(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0,
-                   d_w0, d_u, d_v, d_w):
+                   d_w0, d_tang_x, d_tang_y, d_tang_z, d_tang_x0, d_tang_y0,
+                   d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_u, d_v, d_w):
         d_x0[d_idx] = d_x[d_idx]
         d_y0[d_idx] = d_y[d_idx]
         d_z0[d_idx] = d_z[d_idx]
@@ -31,8 +32,14 @@ class DEMStep(IntegratorStep):
         d_v0[d_idx] = d_v[d_idx]
         d_w0[d_idx] = d_w[d_idx]
 
+        d_tang_x0[d_idx] = d_tang_x[d_idx]
+        d_tang_y0[d_idx] = d_tang_y[d_idx]
+        d_tang_z0[d_idx] = d_tang_z[d_idx]
+
     def stage1(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0, d_w0,
-               d_u, d_v, d_w, d_fx, d_fy, d_fz, d_m_inverse, dt):
+               d_u, d_v, d_w, d_tang_x, d_tang_y, d_tang_z, d_tang_x0,
+               d_tang_y0, d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_fx, d_fy, d_fz,
+               d_m_inverse, dt):
         dtb2 = 0.5 * dt
         d_x[d_idx] = d_x0[d_idx] + dtb2 * d_u[d_idx]
         d_y[d_idx] = d_y0[d_idx] + dtb2 * d_v[d_idx]
@@ -42,8 +49,14 @@ class DEMStep(IntegratorStep):
         d_v[d_idx] = d_v0[d_idx] + dtb2 * d_fy[d_idx] * d_m_inverse[d_idx]
         d_w[d_idx] = d_w0[d_idx] + dtb2 * d_fz[d_idx] * d_m_inverse[d_idx]
 
+        d_tang_x[d_idx] = d_tang_x0[d_idx] + dtb2 * d_vt_x[d_idx]
+        d_tang_y[d_idx] = d_tang_y0[d_idx] + dtb2 * d_vt_y[d_idx]
+        d_tang_z[d_idx] = d_tang_z0[d_idx] + dtb2 * d_vt_z[d_idx]
+
     def stage2(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0, d_w0,
-               d_u, d_v, d_w, d_fx, d_fy, d_fz, d_m_inverse, dt):
+               d_u, d_v, d_w, d_tang_x, d_tang_y, d_tang_z, d_tang_x0,
+               d_tang_y0, d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_fx, d_fy, d_fz,
+               d_m_inverse, dt):
         d_x[d_idx] = d_x0[d_idx] + dt * d_u[d_idx]
         d_y[d_idx] = d_y0[d_idx] + dt * d_v[d_idx]
         d_z[d_idx] = d_z0[d_idx] + dt * d_w[d_idx]
@@ -51,3 +64,7 @@ class DEMStep(IntegratorStep):
         d_u[d_idx] = d_u0[d_idx] + dt * d_fx[d_idx] * d_m_inverse[d_idx]
         d_v[d_idx] = d_v0[d_idx] + dt * d_fy[d_idx] * d_m_inverse[d_idx]
         d_w[d_idx] = d_w0[d_idx] + dt * d_fz[d_idx] * d_m_inverse[d_idx]
+
+        d_tang_x[d_idx] = d_tang_x0[d_idx] + dt * d_vt_x[d_idx]
+        d_tang_y[d_idx] = d_tang_y0[d_idx] + dt * d_vt_y[d_idx]
+        d_tang_z[d_idx] = d_tang_z0[d_idx] + dt * d_vt_z[d_idx]
