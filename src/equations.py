4213a619287514dd7794b8f0e3d381bc451220f5
Dinesh A <adepu.dinesh.a@gmail.com>
2017-05-09 22:37:48 +0530

    changed the formulation to mfix dem, as per normal vectors, and force directions. Secondly, added angular velocity properties to the get_particle_array

--- src/equations.py
+++ src/equations.py
@@ -26,8 +26,10 @@ def get_particle_array_dem(constants=None, **props):
 
     """
 
-    dem_props = ['x0', 'y0', 'z0', 'u0', 'v0', 'w0', 'fx', 'fy', 'fz', 'R',
-                 'm_inverse']
+    dem_props = [
+        'x0', 'y0', 'z0', 'u0', 'v0', 'w0', 'wx ', 'wy', 'wz', 'wx0', 'wy0',
+        'wz0', 'fx', 'fy', 'fz', 'R', 'm_inverse'
+    ]
 
     pa = get_particle_array(constants=constants, additional_props=dem_props,
                             **props)
@@ -38,8 +40,8 @@ def get_particle_array_dem(constants=None, **props):
     #     'tag', 'p'
     # ])
     pa.set_output_arrays([
-        'x', 'y', 'z', 'u', 'v', 'w', 'm', 'p', 'pid',
-        'tag', 'gid', 'fx', 'fy', 'fz'
+        'x', 'y', 'z', 'u', 'v', 'w', 'wx', 'wy', 'wz', 'm', 'p', 'pid', 'tag',
+        'gid', 'fx', 'fy', 'fz'
     ])
     # pa.set_output_arrays([
     #     'x', 'y', 'z', 'u', 'v', 'w', 'm', 'pid', 'gid',
@@ -66,6 +68,7 @@ class LinearSpringForceParticleParticle(Equation):
     """Documentation for LinearSpringForce
 
     """
+
     def __init__(self, dest, sources, k=1e4, ln_e=1.0, m_eff=0.5):
         super(LinearSpringForceParticleParticle, self).__init__(dest, sources)
         self.k = k
@@ -74,8 +77,8 @@ class LinearSpringForceParticleParticle(Equation):
         _tmp = np.sqrt(ln_e2 + np.pi * np.pi)
         self.eta = 2 * np.sqrt(m_eff * self.k) * ln_e / _tmp
 
-    def loop(self, d_idx, d_m, s_idx, d_fx, d_fy, d_fz, VIJ, XIJ, RIJ,
-             d_R, s_R):
+    def loop(self, d_idx, d_m, s_idx, d_fx, d_fy, d_fz, VIJ, XIJ, RIJ, d_R,
+             s_R):
         overlap = 0
 
         if RIJ > 0:
@@ -84,9 +87,9 @@ class LinearSpringForceParticleParticle(Equation):
         if overlap > 0:
             # basic variables: normal vector
             _rij = 1. / RIJ
-            nx = XIJ[0] * _rij
-            ny = XIJ[1] * _rij
-            nz = XIJ[2] * _rij
+            nx = -XIJ[0] * _rij
+            ny = -XIJ[1] * _rij
+            nz = -XIJ[2] * _rij
 
             # conservative force
             # d_fx[d_idx] += 1e2 * overlap * nx
@@ -100,18 +103,19 @@ class LinearSpringForceParticleParticle(Equation):
             # d_fz[d_idx] += -2 * v_n * nz
 
             # conservative damping force at one in single equation
-            v_n = VIJ[0] * nx + VIJ[1] * ny + VIJ[2] * nz
+            v_n = (VIJ[0] * nx + VIJ[1] * ny + VIJ[2] * nz)
             k_multi_overlap = self.k * overlap
             eta_multi_normal_velocity = self.eta * v_n
-            d_fx[d_idx] += (k_multi_overlap - eta_multi_normal_velocity) * nx
-            d_fy[d_idx] += (k_multi_overlap - eta_multi_normal_velocity) * ny
-            d_fz[d_idx] += (k_multi_overlap - eta_multi_normal_velocity) * nz
+            d_fx[d_idx] += (-k_multi_overlap - eta_multi_normal_velocity) * nx
+            d_fy[d_idx] += (-k_multi_overlap - eta_multi_normal_velocity) * ny
+            d_fz[d_idx] += (-k_multi_overlap - eta_multi_normal_velocity) * nz
 
 
 class MakeForcesZero(Equation):
     """Documentation for LinearSpringForce
 
     """
+
     def loop(self, d_idx, d_fx, d_fy, d_fz):
         d_fx[d_idx] = 0
         d_fy[d_idx] = 0
