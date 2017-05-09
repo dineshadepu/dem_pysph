4c301a3abfd144f1f43143b877bc4b39701534d4
Dinesh A <adepu.dinesh.a@gmail.com>
2017-05-10 02:24:31 +0530

    Previous commit didnt complete the total torque implementation. 1. Remaining torque integration is added to equations. 2. Hope I completed It. lets see. DEM torque complete.

--- src/integrator.py
+++ src/integrator.py
@@ -23,7 +23,8 @@ class DEMStep(IntegratorStep):
 
     def initialize(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0,
                    d_w0, d_tang_x, d_tang_y, d_tang_z, d_tang_x0, d_tang_y0,
-                   d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_u, d_v, d_w):
+                   d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_u, d_v, d_w, d_wx,
+                   d_wy, d_wz, d_wx0, d_wy0, d_wz0):
         d_x0[d_idx] = d_x[d_idx]
         d_y0[d_idx] = d_y[d_idx]
         d_z0[d_idx] = d_z[d_idx]
@@ -32,6 +33,10 @@ class DEMStep(IntegratorStep):
         d_v0[d_idx] = d_v[d_idx]
         d_w0[d_idx] = d_w[d_idx]
 
+        d_wx0[d_idx] = d_wx[d_idx]
+        d_wy0[d_idx] = d_wy[d_idx]
+        d_wz0[d_idx] = d_wz[d_idx]
+
         d_tang_x0[d_idx] = d_tang_x[d_idx]
         d_tang_y0[d_idx] = d_tang_y[d_idx]
         d_tang_z0[d_idx] = d_tang_z[d_idx]
@@ -39,7 +44,8 @@ class DEMStep(IntegratorStep):
     def stage1(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0, d_w0,
                d_u, d_v, d_w, d_tang_x, d_tang_y, d_tang_z, d_tang_x0,
                d_tang_y0, d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_fx, d_fy, d_fz,
-               d_m_inverse, dt):
+               d_torX, d_torY, d_torZ, d_wx, d_wy, d_wz, d_wx0, d_wy0, d_wz0,
+               d_m_inverse, d_I_inverse, dt):
         dtb2 = 0.5 * dt
         d_x[d_idx] = d_x0[d_idx] + dtb2 * d_u[d_idx]
         d_y[d_idx] = d_y0[d_idx] + dtb2 * d_v[d_idx]
@@ -49,6 +55,10 @@ class DEMStep(IntegratorStep):
         d_v[d_idx] = d_v0[d_idx] + dtb2 * d_fy[d_idx] * d_m_inverse[d_idx]
         d_w[d_idx] = d_w0[d_idx] + dtb2 * d_fz[d_idx] * d_m_inverse[d_idx]
 
+        d_wx[d_idx] = d_wx0[d_idx] + dtb2 * d_torX[d_idx] * d_I_inverse[d_idx]
+        d_wy[d_idx] = d_wy0[d_idx] + dtb2 * d_torY[d_idx] * d_I_inverse[d_idx]
+        d_wz[d_idx] = d_wz0[d_idx] + dtb2 * d_torZ[d_idx] * d_I_inverse[d_idx]
+
         d_tang_x[d_idx] = d_tang_x0[d_idx] + dtb2 * d_vt_x[d_idx]
         d_tang_y[d_idx] = d_tang_y0[d_idx] + dtb2 * d_vt_y[d_idx]
         d_tang_z[d_idx] = d_tang_z0[d_idx] + dtb2 * d_vt_z[d_idx]
@@ -56,7 +66,8 @@ class DEMStep(IntegratorStep):
     def stage2(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_u0, d_v0, d_w0,
                d_u, d_v, d_w, d_tang_x, d_tang_y, d_tang_z, d_tang_x0,
                d_tang_y0, d_tang_z0, d_vt_x, d_vt_y, d_vt_z, d_fx, d_fy, d_fz,
-               d_m_inverse, dt):
+               d_torX, d_torY, d_torZ, d_wx, d_wy, d_wz, d_wx0, d_wy0, d_wz0,
+               d_m_inverse, d_I_inverse, dt):
         d_x[d_idx] = d_x0[d_idx] + dt * d_u[d_idx]
         d_y[d_idx] = d_y0[d_idx] + dt * d_v[d_idx]
         d_z[d_idx] = d_z0[d_idx] + dt * d_w[d_idx]
@@ -64,6 +75,10 @@ class DEMStep(IntegratorStep):
         d_u[d_idx] = d_u0[d_idx] + dt * d_fx[d_idx] * d_m_inverse[d_idx]
         d_v[d_idx] = d_v0[d_idx] + dt * d_fy[d_idx] * d_m_inverse[d_idx]
         d_w[d_idx] = d_w0[d_idx] + dt * d_fz[d_idx] * d_m_inverse[d_idx]
+
+        d_wx[d_idx] = d_wx0[d_idx] + dt * d_torX[d_idx] * d_I_inverse[d_idx]
+        d_wy[d_idx] = d_wy0[d_idx] + dt * d_torY[d_idx] * d_I_inverse[d_idx]
+        d_wz[d_idx] = d_wz0[d_idx] + dt * d_torZ[d_idx] * d_I_inverse[d_idx]
 
         d_tang_x[d_idx] = d_tang_x0[d_idx] + dt * d_vt_x[d_idx]
         d_tang_y[d_idx] = d_tang_y0[d_idx] + dt * d_vt_y[d_idx]
